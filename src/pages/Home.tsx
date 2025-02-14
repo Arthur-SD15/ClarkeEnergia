14103d06c2dee31f5df589ec2a09a73b840067b5
Arthur Silva Dantas <arthur.dantas@ufms.br>
2025-02-14 10:42:01 -0300

    feat: add initial project configuration with React + Typescript

---
+++ src/pages/Home.tsx
@@ -0,0 +1,8 @@
+const Home = () => {
+    return (
+        <div className="dark:bg-stone-950 h-screen p-[5vh]">
+        </div>
+    );
+};
+
+export default Home;
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: 'media', // Agora, o dark mode será ativado automaticamente com base na preferência do sistema
+  plugins: [],
+}
